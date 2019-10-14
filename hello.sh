cf71eaab6e7a8ceb77e2d7da1e04c84f5757647f
Turar <54972943+TurarGeek@users.noreply.github.com>
2019-10-14 21:14:46 +0600

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,13 +1,1 @@
-TurarGeek@host:~/piscine-go$ ./hello.sh
-Hello kigiri!
-TurarGeek@host:~/piscine-go$
-
-
-
-
-
-    git add hello.sh
-    git commit -m "My very first commit"
-    git push origin master
-
-    
+echo Hello TurarGeek!
