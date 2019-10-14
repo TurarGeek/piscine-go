498021487a7dd15059051b18d773f906cd19d9cb
Turar <54972943+TurarGeek@users.noreply.github.com>
2019-10-14 19:27:12 +0600

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,13 @@
+TurarGeek@host:~/piscine-go$ ./hello.sh
+Hello kigiri!
+TurarGeek@host:~/piscine-go$
+
+
+
+
+
+    git add hello.sh
+    git commit -m "My very first commit"
+    git push origin master
+
+    
